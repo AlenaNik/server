12bb21c67d37789ad149e7aa92c69ccf240a006d
AlenaNik <alena.nik.digital@gmail.com>
2019-06-22 14:51:18 +0200

    append, write, delete created

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ fs.readFile('./hello.txt', (err, data) => {
 
 const file = fs.readFileSync('./hello.txt');
 console.log('Sync', file.toString())
+// APPEND
+// fs.appendFile('./hello.txt', 'This is awesome!', err => {
+// if(err) {
+//     console.log(err)
+// }
+// });
+// WRITE
+// fs.writeFile('bye.txt', 'Bye friend', err => {
+//     if (err) {
+//         console.log(err)
+//     }
+// })
+
+// DELETE
+fs.unlink('./bye.txt', err => {
+    if (err) {
+        console.log(err)
+    }
+    console.log('Bye to bye text')
+})
